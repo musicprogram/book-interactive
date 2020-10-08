c5be1af25aae302d4fdcf395e578e2212295b345
musicprogram <jahenao17@misena.edu.co>
2020-10-08 15:28:23 -0500

    Updates

---
+++ precache-manifest.dc5d39c59989438a007dba6362bc0d53.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04968c407bb6280cb7b13c69c671961",
+    "url": "/book-interactive/index.html"
+  },
+  {
+    "revision": "d3f715678355862d173c",
+    "url": "/book-interactive/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ca1e398d87582829987d",
+    "url": "/book-interactive/static/css/main.dce74188.chunk.css"
+  },
+  {
+    "revision": "d3f715678355862d173c",
+    "url": "/book-interactive/static/js/2.c186fd06.chunk.js"
+  },
+  {
+    "revision": "1baa02233bbd827e3584e32091a75902",
+    "url": "/book-interactive/static/js/2.c186fd06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1e398d87582829987d",
+    "url": "/book-interactive/static/js/main.21963077.chunk.js"
+  },
+  {
+    "revision": "5f2d70484b53199a3552",
+    "url": "/book-interactive/static/js/runtime-main.88a36cc3.js"
+  },
+  {
+    "revision": "ab180487f09eda3ea4462314ceafa979",
+    "url": "/book-interactive/static/media/Design.ab180487.svg"
+  },
+  {
+    "revision": "d29dd310839d67c0730bc4947ae3d689",
+    "url": "/book-interactive/static/media/Drawing.d29dd310.svg"
+  },
+  {
+    "revision": "f9252a3b3ce292a211f00721b1b5cfc6",
+    "url": "/book-interactive/static/media/Support team.f9252a3b.svg"
+  }
+]);
