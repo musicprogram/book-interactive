e855be1756a3621bac274a0e73f5e6c381e41a23
musicprogram <jahenao17@misena.edu.co>
2020-10-08 16:48:48 -0500

    modal only once by session

--- src/components/UI/Init.js
+++ src/components/UI/Init.js
@@ -1,17 +1,29 @@
-import React, {useEffect, createRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Parallax from 'parallax-js'
+import {Modal, Image} from 'react-bootstrap'
 
 import imgFirst from '../../assets/img/Drawing.svg'
 import imgSecond from '../../assets/img/Support team.svg'
 import imgThird from '../../assets/img/Design.svg'
+import team from '../../assets/img/Team.svg'
 
 function Init() {
-  const scene = createRef();
-  const scenec = createRef();
-  const scener = createRef();
+  const scene = useRef();
+  const scenec = useRef();
+  const scener = useRef();
 
+  const [show, setShow] = useState(true);
 
   useEffect(()=>{
+    const modalExist = localStorage.getItem("modalExist");
+    if(modalExist){
+      setShow(false);
+    }else{
+      localStorage.setItem("modalExist", "true");
+    }
+
+
+
     setTimeout(()=>{
       if(scene.current && scenec.current && scener.current){
         const parallax = new Parallax(scene.current);
@@ -55,6 +67,34 @@ function Init() {
         <p className="lead mb-0 color-main font-weight-bold text-hand">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
         <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
       </blockquote>
+
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+
+        <Image src={team} roundedCircle/>
+
+
+        <Modal.Body>
+          <h1 className="color-main text-center font-weight-bold">
+            Welcome!!!
+          </h1>
+          <p>
+            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+            ipsam atque a dolores quisquam quisquam adipisci possimus
+            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+            deleniti rem!
+          </p>
+        </Modal.Body>
+      </Modal>
+
     </div>
 
   );
