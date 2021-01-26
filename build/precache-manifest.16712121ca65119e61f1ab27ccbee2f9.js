9d3e50251e0067cc29eafb514fa40a3588cabc64
juan henao <jahenao17@misena.edu.co>
2021-01-26 11:25:04 -0500

    build

---
+++ build/precache-manifest.16712121ca65119e61f1ab27ccbee2f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778c449fa23bea637418c7d29f1b385b",
+    "url": "/book-interactive/index.html"
+  },
+  {
+    "revision": "a6998a039d37d0132b33",
+    "url": "/book-interactive/static/css/2.b565b473.chunk.css"
+  },
+  {
+    "revision": "3909a48399d7638eb860",
+    "url": "/book-interactive/static/css/main.5a51b51f.chunk.css"
+  },
+  {
+    "revision": "a6998a039d37d0132b33",
+    "url": "/book-interactive/static/js/2.3ad48a4f.chunk.js"
+  },
+  {
+    "revision": "a41506294c22016896304dee19257fed",
+    "url": "/book-interactive/static/js/2.3ad48a4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3909a48399d7638eb860",
+    "url": "/book-interactive/static/js/main.b25c9c62.chunk.js"
+  },
+  {
+    "revision": "5f2d70484b53199a3552",
+    "url": "/book-interactive/static/js/runtime-main.88a36cc3.js"
+  },
+  {
+    "revision": "ca59b9a4f64fe58aedc7849503cb11bf",
+    "url": "/book-interactive/static/media/fondo4.ca59b9a4.png"
+  }
+]);
